1d5b955227c1cccd1e0f7ad858bc4ad349d47d84
vierge <marancholia@gmail.com>
2020-02-06 21:10:06 +0000

    added getUserWithId and addUser sql query functions. sadly, duplicate signups are possible at the moment

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -48,7 +48,16 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+    SELECT * FROM users
+    WHERE id = $1
+  `
+  return pool.query(queryString, [`${id}`])
+  .then(res => {
+    res.rows
+    console.log(res.rows);
+  })
+  .catch(err => console.log('cannot find user!', err.stack));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -59,10 +68,24 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+
+  
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `
+  return pool.query(queryString, [`${user.name}`, `${user.email}`, `${user.password}`])
+  .then(res => {
+    res.rows
+    console.log(`grabbed ${res.rows}`);
+  })
+  .catch(err => console.log(`error error error!!!!`, err.stack));
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
